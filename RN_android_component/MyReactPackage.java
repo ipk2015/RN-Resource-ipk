fc51419636f7b93ac86f14147f39ed07acd7d8d6
ipk2015 <1296064976@qq.com>
2016-04-28 10:51:54 +0800

    Added files via upload

---
+++ RN_android_component/MyReactPackage.java
@@ -0,0 +1,46 @@
+package com.RN0405;
+
+import com.RN0405.NativeModules.MyToastAndroid.MyToastAndroid;
+import com.RN0405.ViewManagers.GestureLockViewManager;
+import com.RN0405.ViewManagers.MyTextViewManager;
+import com.RN0405.ViewManagers.PieChartViewManager;
+import com.RN0405.ViewManagers.PieChartViewManager2;
+import com.caimi.testnativeout.TestNativeOut;
+import com.facebook.react.ReactPackage;
+import com.facebook.react.bridge.JavaScriptModule;
+import com.facebook.react.bridge.NativeModule;
+import com.facebook.react.bridge.ReactApplicationContext;
+import com.facebook.react.uimanager.ViewManager;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+/**
+ * Created by yunpengfeng on 16/4/5.
+ */
+public class MyReactPackage implements ReactPackage {
+    @Override
+    public List<NativeModule> createNativeModules(ReactApplicationContext reactContext) {
+        return Arrays.<NativeModule>asList(
+                new MyToastAndroid(reactContext),
+                new TestNativeOut(reactContext)
+        );
+    }
+
+
+    @Override
+    public List<Class<? extends JavaScriptModule>> createJSModules() {
+        return Collections.emptyList();
+    }
+
+    @Override
+    public List<ViewManager> createViewManagers(ReactApplicationContext reactContext) {
+        return Arrays.<ViewManager>asList(
+                new PieChartViewManager(),
+                new PieChartViewManager2(),
+                new MyTextViewManager(),
+                new GestureLockViewManager()
+        );
+    }
+}
